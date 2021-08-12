c48b16f1cf9e935c01693bad2083c4c74d58a971
patrick1130 <patrick1130@163.com>
2021-08-12 22:11:23 +0800

    1st modify

--- src/script.py
+++ src/script.py
@@ -1,9 +1,14 @@
 import pandas as pd
-import xlwt #处理.xls
-import openpyxl #处理.xlsx
+import xlwt  # 处理.xls
+import openpyxl  # 处理.xlsx
 
 df = pd.read_excel('../data/data.xls')
-print(df.head())
+# print(df.head())
+# print(df.dtypes)
+# shebei_df = df['仪器设备']
+# print(df.iloc[4])
 
+# 使用loc获取列子集
+#subset = df.loc[:,[]]
 
-
+print(df.iloc[6:12,1:11])
