c134f035979e250a69b83f771fbca94a4c9b5b24
patrick1130 <patrick1130@163.com>
2021-08-22 00:35:07 +0800

    1st modify

--- src/script.py
+++ src/script.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # visited_file = '../data/2021.xlsx'
 visited_file = '../data/data.xls'
-df = pd.read_excel(visited_file, keep_default_na=False)  # 不包含默认缺失值
+df = pd.read_excel(visited_file)
 df1c = df.iloc[7:12, 1:2]
 df1v = df.iloc[7:12, 2:3]
 df2c = df.iloc[7:12, 3:4]
@@ -15,10 +16,32 @@ df4c = df.iloc[7:12, 7:8]
 df4v = df.iloc[7:12, 8:9]
 df5c = df.iloc[7:12, 9:10]
 df5v = df.iloc[7:12, 10:11]
-current_concat = pd.concat([df1c, df2c, df3c, df4c, df5c], ignore_index=False, join='inner')
-# voltage_concat = pd.concat([df1v,df2v,df3v,df4v,df5v],ignore_index=False,join='inner')
+df1c.columns = ['1']
+df2c.columns = ['1']
+df3c.columns = ['1']
+df4c.columns = ['1']
+df5c.columns = ['1']
+df1v.columns = ['2']
+df2v.columns = ['2']
+df3v.columns = ['2']
+df4v.columns = ['2']
+df5v.columns = ['2']
+# 连接行数据
+current_concat = pd.concat([df1c, df2c, df3c, df4c, df5c], join='inner')
+voltage_concat = pd.concat([df1v, df2v, df3v, df4v, df5v], join='inner')
+# 删除NA值
+df1 = current_concat.dropna()
+df2 = voltage_concat.dropna()
+# 重置索引
+df1.reset_index(drop=True, inplace=True)
+df2.reset_index(drop=True, inplace=True)
+
+dd1 = df1.abs()  # 取绝对值
+dd2 = df2.abs()
+
+# print(dd1)
+# print(dd2)
 # df1.columns = ['1c','1v','2c','2v','3c','3v','4c','4v','5c','5v'
-print(current_concat)
 
 # current_concat.to_csv('../output/data.csv', index=False)
 # df2 = pd.read_csv('../output/data.csv',keep_default_na=False)
@@ -27,8 +50,11 @@ print(current_concat)
 # x=np.linspace(0.05,10,1000)
 # y=np.random.rand(1000)
 
-# z = np.polyfit(x, y, 2) #np.polyfit(x,y,deg=1) 返回多项式系数
-# a =np.poly1d(z)#生成方程式
-# plt.plot(x,a(x))#拟合曲线
+# z = np.polyfit(x, y, 2)  # np.polyfit(x,y,deg=1) 返回多项式系数
+# a = np.poly1d(z)  # 生成方程式
+# plt.plot(x, a(x))  # 拟合曲线
+
+# plt.scatter(dd1,dd2)
+
 # plt.legend()
 # plt.show()
